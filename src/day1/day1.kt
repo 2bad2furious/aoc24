c73a56af6639e7ec4a9c0e2e32197b4d318b0b0f
2bad2furious <22004964+2bad2furious@users.noreply.github.com>
2024-12-02 16:17:55 +0100

    ✨ Add day 1

---
+++ src/day1/day1.kt
@@ -0,0 +1,46 @@
+package day1
+
+import utils.useLines
+import java.io.File
+import kotlin.math.abs
+
+val inputFile = File("src/day1", "input.txt")
+
+val testContent = """3   4
+4   3
+2   5
+1   3
+3   9
+3   3"""
+
+fun main() {
+    println(part2())
+}
+
+fun part2(): Int {
+    val (firstList, secondList) = getLists()
+    val secondOccurance = secondList.groupingBy { it }.eachCount()
+
+    return firstList.sumOf { it * secondOccurance.getOrDefault(it, 0) }
+}
+
+fun part1(): Int {
+    val (firstList, secondList) = getLists()
+    return firstList.asSequence().sorted()
+        .zip(secondList.sorted().asSequence()) { first, second -> abs(first - second) }
+        .sum()
+}
+
+fun getLists(): Pair<List<Int>, List<Int>> {
+    val input = useLines(inputFile, testContent) {
+        it
+            .map { it.split("\\s+".toRegex()) }
+            .map { it.map { n -> n.toInt() } }
+            .toList()
+    }
+
+    val firstList = input.map { it[0] }
+    val secondList = input.map { it[1] }
+
+    return firstList to secondList
+}
---
+++ src/utils/utils.kt
@@ -0,0 +1,16 @@
+package utils
+
+import java.io.File
+
+fun <R> useLines(file: File, text: String, useFile: Boolean = true, cb: (lines: Sequence<String>) -> R): R {
+    if (useFile) {
+        println("file: ${file.absolutePath}")
+        return file.useLines { cb(it) }
+    }
+
+    return text.lineSequence().let(cb)
+}
+
+fun <T> List<T>.withoutIndex(index: Int): List<T> {
+    return filterIndexed { i, _ -> i != index }
+}
